baaa54f7d8b253be8fd50b2242f06c291e026ec2
Asus <romannikonovich@gmail.com>
2020-10-06 11:28:22 +0300

    done

--- app/src/main/java/com/example/presentation/screens/MainActivity.kt
+++ app/src/main/java/com/example/presentation/screens/MainActivity.kt
@@ -1,44 +1,39 @@
 package com.example.presentation.screens
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.presentation.R
+import com.example.presentation.utils.switch
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+const val TAG_F1 = "f1"
+const val TAG_F2 = "f2"
 
 class MainActivity : AppCompatActivity() {
-    private val fragmentManager = supportFragmentManager
-    private lateinit var fragmentJokes: FragmentJokes
-    private lateinit var fragmentWeb: FragmentWeb
-    private lateinit var textLabel: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        fragmentJokes = FragmentJokes.getInstance(fragmentManager)
-        fragmentWeb = FragmentWeb.getInstance(fragmentManager)
-        textLabel = findViewById(R.id.name_tab)
 
-        savedInstanceState?.let { } ?: run {
-            textLabel.text = this.getText(R.string.tab_jokes)
-            fragmentManager.beginTransaction().add(R.id.container_fragment, fragmentJokes)
-                .commit()
-        }
+        savedInstanceState?.let {}
+            ?: run {
+                supportFragmentManager.switch(R.id.container_fragment, FragmentJokes.getInstance(supportFragmentManager), TAG_F1 )
+            }
 
         findViewById<BottomNavigationView>(R.id.bottomNavigationView).setOnNavigationItemSelectedListener {
             when (it.itemId) {
                 R.id.action_jokes -> {
-                    textLabel.text = this.getText(R.string.tab_jokes)
-                    fragmentManager.beginTransaction()
-                        .replace(R.id.container_fragment, fragmentJokes).commit(); true
+                    supportFragmentManager.switch(R.id.container_fragment, FragmentJokes.getInstance(supportFragmentManager), TAG_F1 )
+                    ; true
                 }
                 else -> {
-                    textLabel.text = this.getText(R.string.tab_api)
-                    fragmentManager.beginTransaction()
-                        .replace(R.id.container_fragment, fragmentWeb).commit(); true
+                    supportFragmentManager.switch(R.id.container_fragment, FragmentWeb.getInstance(supportFragmentManager), TAG_F2 )
+                    ; true
                 }
             }
         }
     }
+
 }
